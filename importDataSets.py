fc246a1892387ddc1b2518ed1f11b14aedd0a5ac
Han Solo <HanSolo@MF.com>
2015-11-04 14:48:41 +0000

    added plottin of derivatives (numpy.diff function)

--- importDataSets.py
+++ importDataSets.py
@@ -19,8 +19,22 @@ x1 = readFileData('frequency_sample1.txt')
 x2 = readFileData('frequency_sample2.txt')
 y1 = readFileData('S21_magn_sample1.txt')
 y2 = readFileData('S21_magn_sample2.txt')
-
+"""
 plt.plot(x1,y1)
 plt.plot(x2,y2)
 plt.xlim
+plt.show()
+"""
+direv = np.diff(y1)
+plt.plot(x1[0:-1],direv)
+plt.plot(x1,y1+1,color='red')
+plt.xlim([5,8])
+plt.grid()
+plt.show()
+
+direv = np.diff(y2)
+plt.plot(x2[0:-1],direv)
+plt.plot(x2,y2+2,color='red')
+plt.xlim([5,8])
+plt.grid()
 plt.show()
